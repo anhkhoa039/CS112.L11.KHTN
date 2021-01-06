69bc09ff9eac7c8179ea43f7da7a1d6ec5dc1ce0
anvq38 <62863568+anvq38@users.noreply.github.com>
2021-01-06 22:29:18 +0700

    Add files via upload

---
+++ week1/BOT.py
@@ -0,0 +1,17 @@
+def BOT():
+    n = int(input())
+    a = [int(x) for x in input().split(" ")]
+    maxsum = -1000000000000000000
+    sum = 0
+    p = 1
+    for i in range(0, n):
+        sum += a[i]
+        if (sum < 0):
+            sum = 0
+            p = i + 2
+        if(maxsum < sum):
+            maxsum = sum
+            star = p
+            end = i + 1
+    print(star,end,maxsum,end=' ')
+BOT()
